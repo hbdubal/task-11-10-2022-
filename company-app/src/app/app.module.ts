9e19f9d6293059fc07724c224dc2875743045cf1
Hiral Dubal <hiral.dubal@1Rintern.local>
2022-10-12 12:05:10 +0530

    Routing

--- company-app/src/app/app.module.ts
+++ company-app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- company-app/src/app/company/company-routing.module.ts
+++ company-app/src/app/company/company-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyFormComponent } from './company-form/company-form.component';
 import { CompanyComponent } from './company.component';
 
-const routes: Routes = [{ path: '', component: CompanyComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: CompanyComponent,
+
+    // Start: Children Routing Path
+    children: [
+      {
+        path: 'add',
+        component: CompanyFormComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: CompanyFormComponent
+      }
+    ]
+    // End: Children Routing Path
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
