0cc738e57479a4da930067e2c9af9785dbf6081a
Hiral Dubal <hiral.dubal@1Rintern.local>
2022-10-13 15:47:44 +0530

    Create List and Get data

--- src/app/company/company-form/company-form.component.ts
+++ src/app/company/company-form/company-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CompanyService } from 'src/app/service/company.service';
 
 @Component({
   selector: 'app-company-form',
@@ -7,11 +9,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./company-form.component.scss']
 })
 export class CompanyFormComponent implements OnInit {
-  public empForm: FormGroup;
+  public companyForm: FormGroup;
   isSubmitted: boolean = false;
 
-  constructor(private fb: FormBuilder) {
-    this.empForm = new FormGroup('');
+  constructor(private fb: FormBuilder, private companyService:CompanyService, private router:Router) {
+    this.companyForm = new FormGroup('');
   }
   //Multiselected Drowpdown List
   tags = [
@@ -27,25 +29,25 @@ export class CompanyFormComponent implements OnInit {
     
 //  Form Validation Method 
   ngOnInit(): void {
-    this.empForm = this.fb.group({
+    this.companyForm = this.fb.group({
       companyName: ['', [Validators.required]],
       companyDescription: ['', [Validators.required]],
       companyTags:['', [Validators.required]]
     })
   }
 
-  //  Submit Data On Button Click Event 
+  //  Submit And Post Data On Button Click Event 
   submit() {
     this.isSubmitted = true;
-    if (this.empForm.valid) {
-      this.empForm.value;
-      console.log(this.empForm.value);
+    if (this.companyForm.valid) {
+      this.companyForm.value;
+      console.log(this.companyForm.value);
     }
   }
 
   //  Reset Data On Button Click Event 
   onReset() {
-    this.empForm.reset();
+    this.companyForm.reset();
   }
 }
 
