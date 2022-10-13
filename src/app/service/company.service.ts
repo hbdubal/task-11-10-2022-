5575de422fe1840a71e1ea516acbafbd2795047d
Hiral Dubal <hiral.dubal@1Rintern.local>
2022-10-13 13:34:08 +0530

    Company-Form Validation

--- src/app/service/company.service.ts
+++ src/app/service/company.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CompanyService {
   public compUrl: string;
 
   constructor(private http: HttpClient) {
-    this.compUrl = "  http://localhost:3000/company";
+    this.compUrl = "http://localhost:3000/company";
   }
-
+  
+ 
 }
